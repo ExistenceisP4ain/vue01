6e7af7f0c2d2f3a992a5ffc029b57ce416249c6c
ExistenceisP4ain <57858720+ExistenceisP4ain@users.noreply.github.com>
2019-11-17 10:05:14 +0100

    Add files via upload

---
+++ src/views/Home.vue
@@ -0,0 +1,11 @@
+<template>
+<div class="jumbotron">
+  <h1 class="display-4">Hello, world!</h1>
+  <p class="lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Massa id neque aliquam vestibulum morbi.</p>
+  <hr class="my-4">
+  <p>Congue mauris rhoncus aenean vel elit. Nunc pulvinar sapien et ligula ullamcorper malesuada proin libero. Aliquam eleifend mi in nulla. Bibendum ut tristique et egestas quis ipsum suspendisse ultrices. Pellentesque habitant morbi tristique senectus et netus et malesuada. Ipsum suspendisse ultrices gravida dictum. Magna ac placerat vestibulum lectus mauris.</p>
+  <p class="lead">
+    <a class="btn btn-primary btn-lg" href="login" role="button">Login</a>
+  </p>
+</div>
+</template>
---
+++ src/views/signup.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="login">
+    <h1>This is a signup page</h1><br>
+    <div class="container">
+  <div class="row">
+    <div class="col-sm">
+    </div>
+    <div class="col-sm">
+      <form>
+          <div class="row">
+    <div class="col">
+      <input type="text" class="form-control" placeholder="First name">
+    </div>
+    <div class="col">
+      <input type="text" class="form-control" placeholder="Last name">
+    </div>
+  </div><br>
+  <div class="form-group">
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+  </div>
+  <div class="form-group">
+    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+  </div>
+  <div class="form-group">
+    <input type="password1" class="form-control" id="exampleInputPassword2" placeholder="Confirm password">
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
+  <small id="emailHelp" class="form-text text-muted">We'll never share your details with anyone else.</small>
+</form>
+    </div>
+    <div class="col-sm">
+    </div>
+  </div>
+</div>
+  </div>
+</template>
