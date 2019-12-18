b41bb8fedb8e97ece01d02c7272fc970a07066c7
ExistenceisP4ain <57858720+ExistenceisP4ain@users.noreply.github.com>
2019-12-18 17:42:54 +0100

    Add files via upload

--- src/main.js
+++ src/main.js
@@ -1,10 +1,26 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import uploader from 'vue-simple-uploader'
+import firebase from 'firebase'
 
-Vue.config.productionTip = false
+Vue.use(uploader)
+
+firebase.initializeApp ({
+  apiKey: "AIzaSyAYM-yPxTuXeS5y6ydoigKufzsptZ8tYtw",
+  authDomain: "vuedz-3a2e9.firebaseapp.com",
+  databaseURL: "https://vuedz-3a2e9.firebaseio.com",
+  projectId: "vuedz-3a2e9",
+  storageBucket: "vuedz-3a2e9.appspot.com",
+  messagingSenderId: "580229607199",
+  appId: "1:580229607199:web:47a17dc8aa23e28dc48a2a",
+  measurementId: "G-TCW7HEWKJD"
+});
 
 new Vue({
   router,
-  render: h => h(App)
+  render: h => h(App),
+  render(createElement){
+    return createElement(App)
+  }
 }).$mount('#app')
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,144 @@
 <template>
-<div class="jumbotron">
-  <h1 class="display-4">Hello, world!</h1>
-  <p class="lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Massa id neque aliquam vestibulum morbi.</p>
-  <hr class="my-4">
-  <p>Congue mauris rhoncus aenean vel elit. Nunc pulvinar sapien et ligula ullamcorper malesuada proin libero. Aliquam eleifend mi in nulla. Bibendum ut tristique et egestas quis ipsum suspendisse ultrices. Pellentesque habitant morbi tristique senectus et netus et malesuada. Ipsum suspendisse ultrices gravida dictum. Magna ac placerat vestibulum lectus mauris.</p>
-  <p class="lead">
-    <a class="btn btn-primary btn-lg" href="login" role="button">Login</a>
-  </p>
-</div>
+  <div class="row">
+    <div class="col-8">
+
+      <div v-if="authenticated">
+        <!-- oof -->
+      <p>Upload an image to Firebase:</p>
+      <input type="file" @change="previewImage" accept="image/*" >
+    
+      
+        <img class="preview" :src="picture">
+        <br>
+      <button @click="onUpload">Upload</button>
+
+        <!-- nova forma za post -->
+ <form @submit.prevent="postNewImage" class="form-inline mb-5">
+ <div class="form-group">
+ <label for="imageUrl">Image URL</label>
+ <input v-model="newImageUrl" type="text" class="form-control ml-2"
+id="imageUrl" placeholder="Enter the image URL">
+ </div>
+ <button type="submit" class="btn btn-primary ml-2">Post image</button>
+ </form>
+
+<!-- drag n drop -->
+<uploader :options="options" class="uploader-example">
+    <uploader-unsupport></uploader-unsupport>
+    <uploader-drop>
+      <p>Drop files here to upload or</p>
+      <uploader-btn>select files</uploader-btn>
+      <uploader-btn :attrs="attrs">select images</uploader-btn>
+      <uploader-btn :directory="true">select folder</uploader-btn>
+    </uploader-drop>
+    <uploader-list></uploader-list>
+  </uploader>
+
+
+  
+
+
+ <!-- listanje postova -->
+ <InstagramCard :key="card.id" :info="card" v-for="card in filteredCards"
+/>
+      </div>
+
+    </div>
+    <div class="col-4">
+      Trazimo sljedece: {{ searchTerm }}
+    </div>
+  </div>
 </template>
+
+<script>
+import InstagramCard from '@/components/InstagramCard.vue'
+import store from '@/store.js'
+import firebase from 'firebase';
+
+export default {
+  name: 'upload',
+  data () {
+    return store;
+    return{
+      imageData: null,
+      picture: null,
+      uploadValue: 0
+	}
+  },
+  computed: {
+    filteredCards () {
+      // let filtered = []
+      // for(let card of this.cards) {
+      //   if (card.title.includes(this.searchTerm)) {
+      //     filtered.push(card)
+      //   }
+      // }
+      // return filtered;
+      return this.cards.filter(card => card.title.includes(this.searchTerm));
+    }
+  },
+  methods: {
+    previewImage(event) {
+      this.uploadValue=0;
+      this.picture=null;
+      this.imageData = event.target.files[0];
+    },
+    onUpload(){
+      this.picture=null;
+      const storageRef=firebase.storage().ref(`${this.imageData.name}`).put(this.imageData);
+      storageRef.on(`state_changed`,snapshot=>{
+        this.uploadValue = (snapshot.bytesTransferred/snapshot.totalBytes)*100;
+      }, error=>{console.log(error.message)},
+      ()=>{this.uploadValue=100;
+        storageRef.snapshot.ref.getDownloadURL().then((url)=>{
+          this.picture =url;
+        });
+      }
+      );
+    },
+      postNewImage(){
+        //console.log("dodajemo novu sliku: ", this.newImageUrl);
+        db.collection("posts").add({
+          url: this.newImageUrl,
+          email: this.userEmail,
+          posted_at: Date.now()
+        })
+      }
+  },
+  name: 'home',
+  components: {
+    InstagramCard
+  }
+}
+</script>
+
+<style lang="scss">
+
+.card-body {
+  padding: 0px;
+}
+
+img:hover {
+  cursor: pointer;
+}
+.uploader-example {
+    width: 880px;
+    padding: 15px;
+    margin: 40px auto 0;
+    font-size: 12px;
+    box-shadow: 0 0 10px rgba(0, 0, 0, .4);
+  }
+  .uploader-example .uploader-btn {
+    margin-right: 4px;
+  }
+  .uploader-example .uploader-list {
+    max-height: 440px;
+    overflow: auto;
+    overflow-x: hidden;
+    overflow-y: auto;
+  }
+
+  img.preview {
+    width: 200px;
+}
+</style>
